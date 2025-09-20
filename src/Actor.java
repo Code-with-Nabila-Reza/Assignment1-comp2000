b9af9aa4a5487dcd03358dbafa575b50c1b20568
Nabila Reza <nabila349d@gmail.com>
2025-09-21 06:09:10 +1000

    Added points system to Actor class with methods to get and modify total points

--- src/Actor.java
+++ src/Actor.java
@@ -6,6 +6,7 @@ public abstract class Actor implements Movable{
   ArrayList<Polygon> shapes = new ArrayList<>();
   protected Cell loc;
   protected Inventory<Collectible> ActInventory = new  Inventory<>();
+  protected int points = 0;
 
   public void paint(Graphics g) {
     for (Polygon P : shapes){
@@ -19,11 +20,18 @@ public abstract class Actor implements Movable{
   }
 
   public abstract boolean canBeMoved(Cell c);
+  public abstract int getPointsEarnedFromCell(Cell c);
 
   @Override
   public void move(Cell destinationCell, Grid grid){
     if(grid.cellIsInsideGrid(destinationCell) && canBeMoved(destinationCell)){
-       this.loc = destinationCell;
+      //each cell has points based on its type
+      //each cell has collectibles that have their own points
+      int newCellPoint = getPointsEarnedFromCell(destinationCell);
+
+
+
+      this.loc = destinationCell;
     }
   }
 
@@ -33,6 +41,18 @@ public abstract class Actor implements Movable{
 
   public Inventory<Collectible> getInventory(){
     return ActInventory;
+  }
+
+  public int getPoints(){
+    return points + ActInventory.getTotalPoints();
+  }
+
+  public void modifyPoints(int p){
+    points += p;
+    //p can be negative depends on the type of cell and its points
+    if(p<0){
+      p=0; // points aren't gonna be zero
+    }
   }
 
 }
