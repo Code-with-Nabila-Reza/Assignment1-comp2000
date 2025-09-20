588cf7a7f0fa3371f0616e9291a89ac429a4b2e9
Nabila Reza <nabila349d@gmail.com>
2025-09-21 05:49:21 +1000

    Added Inventory of collectibles to Actor class and collectItem method to add Collectibles

--- src/Actor.java
+++ src/Actor.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public abstract class Actor implements Movable{
   ArrayList<Polygon> shapes = new ArrayList<>();
   protected Cell loc;
+  protected Inventory<Collectible> ActInventory = new  Inventory<>();
 
   public void paint(Graphics g) {
     for (Polygon P : shapes){
@@ -24,6 +25,14 @@ public abstract class Actor implements Movable{
     if(grid.cellIsInsideGrid(destinationCell) && canBeMoved(destinationCell)){
        this.loc = destinationCell;
     }
+  }
+
+  public void collectItem(Collectible item){
+    ActInventory.addItem(item);
+  }
+
+  public Inventory<Collectible> getInventory(){
+    return ActInventory;
   }
 
 }
