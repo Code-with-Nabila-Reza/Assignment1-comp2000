69cac4b5dcd18087b74ab3332d5fc525801c4279
Nabila Reza <nabila349d@gmail.com>
2025-09-21 22:32:03 +1000

    removed collectible item list printing logic from Stage.java

--- src/Stage.java
+++ src/Stage.java
@@ -45,17 +45,7 @@ public class Stage {
         g.drawString("Items: " + actor.getInventory().size(), 730, yPosition);
         yPosition += 20;
 
-       // displaying each item
-    if (actor.getInventory().size() == 0) {
-      g.drawString("None", 730, yPosition);
-      yPosition += 20;
-  } 
-  else {
-      for (Collectible item : actor.getInventory().getItemList()) {
-          g.drawString(item.getName(), 730, yPosition);
-          yPosition += 20;
-      }
-  }
+      
         //space before next actor
         yPosition += 20;
     }
